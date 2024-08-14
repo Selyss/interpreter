fdbeeffce7ae9bb76ff21f27e600f0101e305ac2
Selyss <99344963+Selyss@users.noreply.github.com>
2024-08-14 00:16:04 +0000

    token

---
+++ python/token.py
@@ -0,0 +1,20 @@
+class TokenType:
+    ILLEGAL = "ILLEGAL"
+    EOF = "EOF"
+    IDENT = "IDENT"
+    INT = "INT"
+    ASSIGN = "="
+    PLUS = "+"
+    COMMA = ","
+    SEMICOLON = ";"
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+    FUNCTION = "FUNCTION"
+    LET = "LET"
+
+class Token:
+    def __init__(self, type_, literal):
+        self.type = type_
+        self.literal = literal
