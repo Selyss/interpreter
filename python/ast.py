40669bbd7d506f9d81e24ba825d759cf3daf6c84
Selyss <99344963+Selyss@users.noreply.github.com>
2024-08-21 01:56:37 +0000

    program node

--- python/ast.py
+++ python/ast.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 class Node(ABC):
     @abstractmethod
@@ -14,3 +15,13 @@ class Expression(Node):
     @abstractmethod
     def expression_node(self):
         pass
+
+class Program(Node):
+    def __init__(self):
+        self.statements: List[Statement] = []
+
+    def token_literal(self) -> str:
+        if len(self.statements) > 0:
+            return self.statements[0].token_literal()
+        else:
+            return ""
